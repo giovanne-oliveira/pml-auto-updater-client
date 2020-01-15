8bcade11758679f31339692725d0747657224e06
Giovanne <giovanne@pm.me>
2020-01-15 14:16:57 -0300

    Init

---
+++ src/Exceptions/DownloadException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PHPMyLicense v4.x
+ *
+ * https://phpmylicense.us/v4
+ *
+ * DownloadException.php
+ */
+
+namespace PHPMyLicense\Exceptions;
+
+use Exception;
+
+class DownloadException extends Exception {}
---
+++ src/Exceptions/ParserExceptions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PHPMyLicense v4.x
+ *
+ * https://phpmylicense.us/v4
+ *
+ * ParserExceptions.php
+ */
+
+namespace PHPMyLicense\Exceptions;
+use Exception;
+
+class ParserException extends Exception {}
